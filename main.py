c2034e629ae2180ceb1d7c726e0e4e969784f4e2
damodara2006 <damodara2006@gmail.com>
2026-01-17 18:02:50 +0530

    feat : updated post

--- main.py
+++ main.py
@@ -2,30 +2,19 @@ from flask import Flask, request
 import os
 
 app = Flask(__name__)
-
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "myVerifyToken123")
 
-@app.get("/webhook")
-def verify():
-    if request.args.get("hub.verify_token") == VERIFY_TOKEN:
-        return request.args.get("hub.challenge"), 200
-    return "Invalid token", 403
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    # ✅ Verify
+    if request.method == "GET":
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge"), 200
+        return "Invalid token", 403
 
-@app.post("/webhook")
-def events():
-    data = request.get_json()
-    print("Got data",data)
-    try:
-        value = data["entry"][0]["changes"][0]["value"]
-        name = value["contacts"][0]["profile"]["name"]
-        wa_id = value["contacts"][0]["wa_id"]
-        text = value["messages"][0]["text"]["body"]
-
-        print(f"✅ New message from {name} ({wa_id}): {text}")
-
-    except Exception as e:
-        print("⚠️ Unknown webhook format:", data)
-
+    # ✅ Receive all events (messages + statuses)
+    data = request.get_json(silent=True)
+    print("✅ WEBHOOK RECEIVED:", data)
     return "OK", 200
 
 @app.get("/")
