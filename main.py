7c673b06a4580673fa748bc9a3f16e1ff04e197b
damodara2006 <damodara2006@gmail.com>
2026-01-17 18:18:29 +0530

    feat : updated post

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ import os
 app = Flask(__name__)
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "myVerifyToken123")
 
-@app.route("/webhook", methods=["GET", "POST"])
+@app.route("/webhook", methods=["GET", "POST"], strict_slashes=False)
 def webhook():
-    # ✅ Verify
     if request.method == "GET":
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return request.args.get("hub.challenge"), 200
         return "Invalid token", 403
 
-    # ✅ Receive all events (messages + statuses)
-    data = request.get_json(silent=True)
-    print("✅ WEBHOOK RECEIVED:", data)
+    print("✅ WEBHOOK HIT:", request.data.decode("utf-8"))
     return "OK", 200
+
 
 @app.get("/")
 def home():
